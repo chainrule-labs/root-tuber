d568d7d9a7acb0df5c8780ac3376c45e2381c70c
0xernesto <78889960+0xernesto@users.noreply.github.com>
2024-05-31 05:46:48 -0500

    feat: add approval logic

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,9 +4,15 @@ import Approval from "../../components/Approval";
 import { isTestnet } from "../../utils/isTestnet";
 import { getBlockExplorerUrl } from "../../utils/getBlockExplorerUrl";
 import Portfolio from "../../components/Portfolio";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useReadContract } from "wagmi";
+import { erc20Abi, zeroAddress } from "viem";
 
 const Home = () => {
-	const { chainId, chain, address } = useAccount();
+	const [isApproved, setIsApproved] = useState(false);
+	const [allowance, setAllowance] = useState(BigInt(0));
+	const { chainId, address } = useAccount();
 
 	const rifName = isTestnet(chainId as number) ? "tRIF" : "RIF";
 	const rifAddress = isTestnet(chainId as number)
@@ -18,19 +24,42 @@ const Home = () => {
 	// Dummy locker address
 	const lockerAddress = "0xDe076D651613C7bde3260B8B69C860D67Bc16f49";
 
+	const { data: allowanceResult } = useReadContract({
+		abi: erc20Abi,
+		address: rifAddress,
+		functionName: "allowance",
+		args: [address || zeroAddress, rootTuberAgentAddress],
+	});
+
+	useEffect(() => {
+		if (address && allowanceResult) {
+			const result = allowanceResult;
+			console.log("allowance", result);
+			setAllowance(result);
+
+			if (result > BigInt(0)) {
+				setIsApproved(true);
+			}
+		}
+	}, [address, isApproved, rifAddress, allowanceResult]);
+
 	return (
 		<div className="flex w-full pt-10 flex-1 flex-col">
-			{/* <Approval
-				rifName={rifName}
-				rifAddress={rifAddress}
-				rootTuberAgentAddress={rootTuberAgentAddress}
-			/> */}
-			<Portfolio
-				lockerAddress={lockerAddress}
-				blockExplorerUrl={blockExplorerUrl}
-				rifName={rifName}
-				rifAddress={rifAddress}
-			/>
+			{isApproved && allowance > BigInt(0) ? (
+				<Portfolio
+					lockerAddress={lockerAddress}
+					blockExplorerUrl={blockExplorerUrl}
+					rifName={rifName}
+					rifAddress={rifAddress}
+				/>
+			) : (
+				<Approval
+					rifName={rifName}
+					rifAddress={rifAddress}
+					rootTuberAgentAddress={rootTuberAgentAddress}
+					setIsApproved={setIsApproved}
+				/>
+			)}
 		</div>
 	);
 };
--- components/Approval.tsx
+++ components/Approval.tsx
@@ -1,29 +1,66 @@
 "use client";
 import Image from "next/image";
-import { erc20Abi } from "viem";
 import { useWriteContract } from "wagmi";
-import { maxInt256 } from "viem";
+import { erc20Abi, maxInt256, checksumAddress } from "viem";
+import { useState } from "react";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 export interface IApproval {
 	rifName: string;
 	rifAddress: `0x${string}`;
 	rootTuberAgentAddress: `0x${string}`;
+	setIsApproved: (isApproved: boolean) => void;
 }
 
 const Approval = ({
 	rifName,
 	rifAddress,
 	rootTuberAgentAddress,
+	setIsApproved,
 }: IApproval) => {
-	const { writeContract } = useWriteContract();
+	const [isLoading, setIsLoading] = useState(false);
+	const { writeContractAsync } = useWriteContract();
+
+	const approve = async () => {
+		setIsLoading(true);
+
+		try {
+			await writeContractAsync({
+				abi: erc20Abi,
+				address: checksumAddress(rifAddress),
+				functionName: "approve",
+				args: [rootTuberAgentAddress, maxInt256],
+				// gasPrice: BigInt(75800000),
+				// gas: BigInt(52993),
+				// type: "legacy",
+			});
+
+			setIsApproved(true);
+		} catch (error) {
+			const acceptableErrorMessages = [
+				"rejected",
+				"request reset",
+				"denied",
+			];
+			if (
+				!acceptableErrorMessages.some((msg) =>
+					(error as Error).message.includes(msg)
+				)
+			) {
+				console.error(error);
+			}
+		}
+
+		setIsLoading(false);
+	};
 
 	return (
-		<div className="flex w-full flex-col">
+		<div className="flex w-full flex-col items-center">
 			<h1 className="text-lg max-w-sm w-full">
 				Approve Root Tuber to save on your behalf every time you spend
 				or receive RIF.
 			</h1>
-			<div className="flex mt-10 items-center">
+			<div className="flex mt-10 max-w-sm w-full items-center">
 				<Image
 					src="/iconRif.svg"
 					alt="RIF Token Icon"
@@ -32,19 +69,18 @@ const Approval = ({
 				/>
 				<span className="text-lg ml-2 mr-6">{rifName}</span>
 				<button
-					className="bg-primary-100 hover:bg-primary-200 h-10 w-28 rounded-full text-lg"
-					onClick={() =>
-						writeContract({
-							abi: erc20Abi,
-							address: rifAddress,
-							functionName: "approve",
-							args: [rootTuberAgentAddress, maxInt256],
-							gasPrice: BigInt(75800000),
-							gas: BigInt(52993),
-						})
-					}
+					className="bg-primary-100 hover:bg-primary-200 h-10 w-28 rounded-full flex items-center justify-center text-lg"
+					onClick={() => approve()}
+					disabled={isLoading}
 				>
-					Approve
+					{isLoading ? (
+						<AiOutlineLoading3Quarters
+							className="animate-spin"
+							size={20}
+						/>
+					) : (
+						"Approve"
+					)}
 				</button>
 			</div>
 		</div>
